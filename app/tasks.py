b51d8cefd09d0a4d8aa40d07fc745ca7a25f10a9
smdavid16 <soisun.minadavid@gmail.com>
2026-04-28 18:22:13 +0300

    adaugat nvidia-smi

--- app/tasks.py
+++ app/tasks.py
@@ -2,17 +2,29 @@ import os
 import subprocess
 from celery import shared_task
 
+def are_nvidia_gpu():
+    """Verifică dacă sistemul are un GPU NVIDIA accesibil."""
+    try:
+        subprocess.check_output(["nvidia-smi", "-L"], stderr=subprocess.STDOUT)
+        return True
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return False
+
 @shared_task(bind=True)
 def proceseaza_melodia_task(self, nume_fisier, cale_folder_melodii):
     cale_abs = os.path.abspath(cale_folder_melodii)
     
-    comanda = [
-        "docker", "run", "--rm",
+    comanda = ["docker", "run", "--rm"]
+    
+    if are_nvidia_gpu():
+        comanda.extend(["--gpus", "all"])
+    
+    comanda.extend([
         "-v", f"{cale_abs}:/app",
         "stemcomposer",
         "demucs",
         nume_fisier
-    ]
+    ])
     
     proces = subprocess.Popen(comanda, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
     
--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+python3 -m venv venv
+source venv/bin/activate
+pip install django celery redis djangorestframework
 echo "Pornim Redis, Celery si Django..."
 sudo systemctl start redis-server
-source venv/bin/activate
+
 
 celery -A core worker --loglevel=warning &
 
