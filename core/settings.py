8c4bd6802e647d43ee977f34a58672a64a15bbf4
Toma <toma.jianu@gmail.com>
2026-06-15 14:58:47 +0300

    Adaugare DEFAULT_AUTO_FIELD pentru Django 6

--- core/settings.py
+++ core/settings.py
@@ -132,3 +132,6 @@ CELERY_TASK_SERIALIZER = 'json'
 # Media files (uploaded audio)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
